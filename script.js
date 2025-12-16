3030a28a2c6c7cf793463dd1883daa4494c1890f
Sara Salzano <sarasalzano95@gmail.com>
2025-12-16 16:18:25 +0100

    esegui snack 3

--- script.js
+++ script.js
@@ -73,3 +73,24 @@ const discountedBooks = availableBooks.map((a) => {
 });
 const fullPricedBook = discountedBooks[0];
 console.log(discountedBooks);
+
+// Snack 3 - Ordinare gli Autori
+// Creare un array (authors) che contiene gli autori dei libri.
+// Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
+// Ordina l’array authors in base all’età, senza creare un nuovo array.
+// (se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+
+const authors = books.map((b) => b.author);
+const areAuthorsAdults = authors.every((a) => a.age > 18);
+console.log(areAuthorsAdults);
+
+authors.sort((a, b) => {
+  let sortType;
+  if (areAuthorsAdults === true) {
+    sortType = b.age - a.age;
+  } else {
+    sortType = a.age - b.age;
+  }
+  return sortType;
+});
+console.log(authors);
