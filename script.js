b402a64f0e77289c4a0b8bfda995db51809007ea
Sara Salzano <sarasalzano95@gmail.com>
2025-12-16 16:17:59 +0100

    esegui snack 2

--- script.js
+++ script.js
@@ -56,3 +56,20 @@ const longBooksTitles = longBooks.map((l) => l.title);
 console.log(longBooksTitles);
 longBooksTitles.forEach((l) => console.log(l));
 
+// Snack 2 - Il primo libro scontato
+// Creare un array (availableBooks) che contiene tutti i libri disponibili.
+// Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+//Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+
+const availableBooks = books.filter((b) => b.available === true);
+console.log(availableBooks);
+
+const discountedBooks = availableBooks.map((a) => {
+  const discount = (parseFloat(a.price) * 0.8).toFixed(2) + "€";
+  return {
+    ...a,
+    price: discount,
+  };
+});
+const fullPricedBook = discountedBooks[0];
+console.log(discountedBooks);
